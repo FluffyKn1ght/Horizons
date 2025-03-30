636e5bc326fe82fb2d7f661b520a96aca9ceae20
LonelyFear <152335919+LonelyFear@users.noreply.github.com>
2025-03-30 12:05:12 -0400

    Added Weapon and Projectile Data types

--- Scripts/HealthComponent.gd
+++ Scripts/HealthComponent.gd
@@ -1,5 +1,4 @@
 extends Node
-
 # An object that stores health data
 class_name HealthComponent
 
@@ -8,13 +7,7 @@ signal Dead # Gets emitted when health falls to 0
 @export var health : int = 10 # Current health level
 @export var maxHealth : int = 10 # Max health level
 
-var process : bool = true # Whether to process death
-
-func _process(delta: float) -> void:
-	# Turn on processing if our health is above 0
-	if health and not process:
-		process = true
-	# If our health falls below 0, emit signal Dead and turn processing off
-	if health <= 0 and process:
+func changeHp(amount : int):
+	health -= amount
+	if (health <= 0 ):
 		Dead.emit()
-		process = false
